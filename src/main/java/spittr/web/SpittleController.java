8762d15453a5665f679c93c34668c408c40ecf06
haizhiship <haizhiship@163.com>
2017-10-11 21:27:27 +0800

    第五章
    Spittle测试分页

--- src/main/java/spittr/web/SpittleController.java
+++ src/main/java/spittr/web/SpittleController.java
@@ -6,13 +6,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import spittr.Spittle;
 import spittr.data.SpittleRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
 
 	private SpittleRepository spittleRepository;
+	private static final String MAX_LONG_AS_STRING="9223372036854775807";
 
 	@Autowired
 	public SpittleController(SpittleRepository spittleRepository) {
@@ -23,6 +28,13 @@ public class SpittleController {
 	public String spittles(Model model) {
 		model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
 		return "spittles";
+	}
 
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Spittle> spittles(
+			@RequestParam(value = "max",
+			defaultValue = MAX_LONG_AS_STRING) long max,
+			@RequestParam(value = "count",defaultValue = "20") int count){
+		return spittleRepository.findSpittles(max,count);
 	}
 }
--- src/test/java/spittr/web/SpittleControllerTest.java
+++ src/test/java/spittr/web/SpittleControllerTest.java
@@ -35,6 +35,23 @@ public class SpittleControllerTest {
 				.andExpect(model().attributeExists("spittleList"))
 				.andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
 	}
+	@Test
+	public void shouldShowPagedSpittles() throws Exception{
+		List<Spittle> expectedSpittles = createSpittleList(50);
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findSpittles(238900,50)).thenReturn(expectedSpittles);
+
+
+
+		SpittleController controller=new SpittleController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller)
+				.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+				.build();
+		mockMvc.perform(get("/spittles?max=238900&count=50"))
+				.andExpect(view().name("spittles"))
+				.andExpect(model().attributeExists("spittleList"))
+				.andExpect(model().attribute("spittleList",hasItems(expectedSpittles.toArray())));
+	}
 
 	private List<Spittle> createSpittleList(int count) {
 		// TODO Auto-generated method stub
